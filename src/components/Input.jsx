419de98b2b66ca819cb8d26c76209145dbb179e5
nazish shaheen <156086159+nazishshaheen@users.noreply.github.com>
2024-05-31 10:29:38 +0000

    added update and edit functionallity

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,20 +1,55 @@
 import React, { useEffect, useState } from 'react'
+import toast, { Toaster } from 'react-hot-toast';
 
+export default function Input(props) {
+    const [task, setTask] = useState('');
 
+    const inputChange = (event) => {
+        setTask(event.target.value);
+    }
 
+    const submit = (event) => {
+        event.preventDefault();
+        if (props.dataObj.index > -1) {
+            props.editTask(props.dataObj.index, task);
+            return;
+        }
+        if (task) {
+            props.addTask(task);
+            setTask('');
+            toast.success('Task added Successfully');
+        } else {
+            toast.error('Plzz Enter Your Task');
+        }
 
-
-export default function Input() {
+    }
+    useEffect(() => {
+        setTask(props.dataObj.data);
+        document.querySelector('#input-elm').focus();
+    }, [props.dataObj.data, props.dataObj.index]);
 
     return (
         <section className='mt-3'>
-            <form>
+            <form onSubmit={submit}>
                 <div className='row'>
                     <div className='col-10'>
-                        <input type="text" className="form-control" placeholder='Enter Your Task here' id="input-elm" />
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder='Enter Your Task here'
+                            id="input-elm"
+                            value={task}
+                            onChange={inputChange}
+                        />
                     </div>
                     <div className='col-2 d-grid gap-2'>
-                        <button className='btn outline-none btn-primary' id='btn'>{"Add"}</button>
+                        <button
+                            className='btn outline-none btn-primary'
+                            id='btn'>
+                            {
+                                props.dataObj.index === -1 ? "Add" : "Update"
+                            }
+                        </button>
                     </div>
                 </div>
             </form>
